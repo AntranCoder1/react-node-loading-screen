aa79645e804155fb55cd2d54954f6171a1a6a6d1
thanhantran1 <thanhantran21@gmail.com>
2021-12-11 17:17:57 +0700

    Fetch data in Feed.js on client side

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Post from '../post/Post';
 import './Feed.css';
+import axios from 'axios';
 
 const Feed = () => {
+
+    const [video, setVideo] = useState([]);
+
+    useEffect(() => {
+        const getVideo = async () => {
+            try {
+                const res = await axios.get("/api/videos/1");
+                setVideo(res.data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getVideo();
+    }, [video]);
+
     return (
         <div className="feed">
-            <Post />
+            { video.map((item) => (
+                <Post key={item.id} video={item}/>
+            )) }
         </div>
     )
 }
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Post.css';
 
-const Post = () => {
+const Post = ({ video }) => {
     return (
         <div className="post">
             <div className="postImgContainer">
-                <img src="https://i.ibb.co/MGBtHF4/3.png" alt="" className="postImg" />
-                <span className="postDuration">8:00:00</span>
+                <img src={video.img} alt="" className="postImg" />
+                <span className="postDuration">{video.duration}</span>
             </div>
             <div className="postInfoContainer">
-                <img className="postAvatar" src="https://images.pexels.com/photos/3149036/pexels-photo-3149036.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                <img className="postAvatar" src={video.channel.avatar} alt="" />
                 <div className="postInfo">
-                    <span className="postTitle">Watch my amazing day in job</span>
-                    <span className="postChannel">It Professor</span>
-                    <span className="postDetail">420K views • 2 days ago</span>
+                    <span className="postTitle">{video.title}</span>
+                    <span className="postChannel">{video.channel.name}</span>
+                    <span className="postDetail">{video.views} views • {video.date}</span>
                 </div>
             </div>
         </div>
