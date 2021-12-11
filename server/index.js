124218a65633f01b5f6a5676cc97bc0b9d19dbd8
thanhantran1 <thanhantran21@gmail.com>
2021-12-11 15:19:36 +0700

    Get user and video on server side

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const { user, videos } = require('./fakeData');
+
+app.use(express.json());
+
+app.get("/api/user/1", (req, res) => {
+    setTimeout(() => {
+        res.json(user);
+    }, 3000);
+});
+
+app.get("/api/videos/1", (req, res) => {
+    setTimeout(() => {
+        res.json(videos);
+    }, 5000);
+});
+
 const port = 5000;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
 })
